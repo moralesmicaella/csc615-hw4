e4b10b6919cdc72ba1e1cbc47ebf811cab85bcdf
Micaella Morales <morales.micaella@gmail.com>
2020-04-22 02:11:20 -0700

    updated

--- Encoder.c
+++ Encoder.c
@@ -21,6 +21,7 @@ void getSpeed() {
             if (signal == 1) {
                 pulses++;
                 printf("Number of pulses: %d \n", pulses);
+                while(signal == 1);
             } 
             time_elapsed = millis() - start;
         }
